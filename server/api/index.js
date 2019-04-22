1c25d37e7ffb36dc2f879c9e03a372d99024318a
rancollier <rcollier@magento.com>
2019-04-22 14:50:54 -0500

    clean up code

--- server/api/index.js
+++ server/api/index.js
@@ -1,13 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const bodyParser = require("body-parser");
-const db = require("../db/models");
-const mysql = require("mysql2");
-const User = require("../db/models/user");
-const Users = User(db.sequelize, db.Sequelize);
-const Product = require("../db/models/product");
-const Products = Product(db.sequelize, db.Sequelize);
 const queries = require("../db/queries");
+
 router.use(bodyParser.json());
 // middleware that is specific to this router
 router.use(function timeLog(req, res, next) {
@@ -20,27 +15,31 @@ router.get("/", function(req, res) {
 });
 
 router.get("/users", function(req, res) {
-    Users.findAll().then(users => res.json(users));
+    return queries.getAllUsers().then(users => {
+        res.json(users);
+    });
+});
+
+router.get("/users/:userId", (req, res) => {
+    return queries.getUserById(req.params).then(response => {
+        res.json(response);
+    });
 });
 
 router.get("/prods", function(req, res) {
-    queries.getAllUsers().then(users => {
-        res.json(users);
+    queries.getAllProducts().then(products => {
+        res.json(products);
     });
-    // db.sequelize.query("SELECT * FROM `products`").then(users => {
-    //     res.json(users);
-    // });
 });
 
-router.get("/prods/:byUserId", function(req, res) {
-    // res.send("xxx home page");
-    // Users.findAll().then(users => res.json(users));
-    // Products.findAll().then(prods => res.json(prods));
-
-    db.sequelize
-        .query("SELECT * FROM `products` where userId = 1")
-        .then(users => {
-            res.json(users);
+router.get("/prods/:userId", function(req, res) {
+    return queries
+        .getProductsByUserId(req.params)
+        .then(response => {
+            res.json(response);
+        })
+        .catch(err => {
+            res.send(err);
         });
 });
 
--- server/db/queries.js
+++ server/db/queries.js
@@ -2,17 +2,33 @@ const db = require("./models");
 const queries = {
     getAllUsers: () => {
         return db.sequelize
-            .query("SELECT * FROM `products`")
+            .query("SELECT * FROM users", {
+                type: db.sequelize.QueryTypes.SELECT
+            })
             .then(users => users);
     },
-    getUserById: (id = "") => {
+    getUserById: ({ userId = "" }) => {
         return db.sequelize
-            .query("SELECT * FROM `products` where userId = :userId", {
-                replacements: { userId: id },
-                type: sequelize.QueryTypes.SELECT
+            .query("SELECT * FROM users WHERE id=:userId", {
+                replacements: { userId },
+                type: db.sequelize.QueryTypes.SELECT
             })
-            .then(users => users)
-            .catch(err);
+            .then(results => results);
+    },
+    getAllProducts: () => {
+        return db.sequelize
+            .query("SELECT * FROM products", {
+                type: db.sequelize.QueryTypes.SELECT
+            })
+            .then(products => products);
+    },
+    getProductsByUserId: ({ userId }) => {
+        return db.sequelize
+            .query("SELECT * FROM products WHERE userId=:userId", {
+                replacements: { userId },
+                type: db.sequelize.QueryTypes.SELECT
+            })
+            .then(products => products);
     }
 };
 
