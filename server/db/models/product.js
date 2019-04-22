1711653008c8aba8a2924d41cb627adeaa7a65db
rancollier <rcollier@magento.com>
2019-04-22 14:53:03 -0500

    clean up dead code

--- server/db/models/product.js
+++ server/db/models/product.js
@@ -14,30 +14,3 @@ module.exports = (sequelize, DataTypes) => {
     };
     return Product;
 };
-
-// id: {
-//     allowNull: false,
-//         autoIncrement: true,
-//             primaryKey: true,
-//                 type: Sequelize.INTEGER
-// },
-// title: {
-//     allowNull: false,
-//         type: Sequelize.STRING(100)
-// },
-// description: {
-//     type: Sequelize.STRING(1000)
-// },
-// version: {
-//     allowNull: false,
-//         type: Sequelize.STRING(12)
-// },
-// cost: {
-//     type: Sequelize.FLOAT(5, 2)
-// },
-// eqpStatus: {
-//     type: Sequelize.STRING
-// },
-// userId: {
-//     type: Sequelize.STRING
-// }
